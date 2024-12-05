df25fca61d7893b56e383437b19b7113c5df4e1d
Sean McQuay <srmcquay@gmail.com>
2024-12-05 11:53:52 +0000

    Added individual deployment steps.

--- functions/alive.js
+++ functions/alive.js
@@ -1,11 +1,15 @@
-const handler = async () => ({
-    body: JSON.stringify({
-        isAlive: true
-    }),
-    headers: {
-        'Access-Control-Allow-Origin': '*'
-    },
-    statusCode: 200
-});
+const handler = async () => {
+    console.log('Going to return alive...');
+
+    return ({
+        body: JSON.stringify({
+            isAlive: true
+        }),
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        statusCode: 200
+    });
+};
 
 export default handler;
